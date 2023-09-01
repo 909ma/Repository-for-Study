82ba3ea24b19a619d6aa73416a48ed813c1f45ec
KIM Sinhyuk <90695479+909ma@users.noreply.github.com>
2023-09-01 14:07:46 +0900

    Create Python_소문자로 바꾸기.py

---
+++ 프로그래머스/Python_소문자로 바꾸기.py
@@ -0,0 +1,43 @@
+def solution(myString):
+    answer = myString.lower()
+    return answer
+
+
+print(solution("aBcDeFg"))
+print(solution("aaa"))
+
+
+"""
+def solution(myString):
+    myString = myString.replace("A",'a')
+    myString =myString.replace("B",'b')
+    myString =myString.replace("C",'c')
+    myString =myString.replace("D",'d')
+    myString =myString.replace("E",'e')
+    myString =myString.replace("F",'f')
+    myString =myString.replace("G",'g')
+    myString =myString.replace("H",'h')
+    myString =myString.replace("I",'i')
+    myString =myString.replace("J",'j')
+    myString =myString.replace("K",'k')
+    myString =myString.replace("L",'l')
+    myString =myString.replace("M",'m')
+    myString =myString.replace("N",'n')
+    myString =myString.replace("O",'o')
+    myString =myString.replace("P",'p')
+    myString =myString.replace("Q",'q')
+    myString =myString.replace("R",'r')
+    myString =myString.replace("S",'s')
+    myString =myString.replace("T",'t')
+    myString =myString.replace("U",'u')
+    myString =myString.replace("V",'v')
+    myString =myString.replace("W",'w')
+    myString =myString.replace("X",'x')
+    myString =myString.replace("Y",'y')
+    myString =myString.replace("Z",'z')
+
+    return myString
+
+
+페르마가 보면 까무러치겠네 ㅋㅋㅋㅋ
+"""
