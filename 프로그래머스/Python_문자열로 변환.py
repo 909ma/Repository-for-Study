9c2740688647a8b5b48588bf43f698db33b8f9ce
KIM Sinhyuk <90695479+909ma@users.noreply.github.com>
2023-09-01 14:12:06 +0900

    Create Python_문자열로 변환.py

---
+++ 프로그래머스/Python_문자열로 변환.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = str(n)
+    return answer
+
+
+print(solution(123))
+print(solution(2573))
+
+
+"""
+def solution(n):
+    answer = f'{n}'
+    return answer
+
+
+이 코드 재밌네
+"""
