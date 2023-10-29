4bf0f66eb0757b7a771e2ade5db21cba3d2eb866
909ma <sin00021@naver.com>
2023-10-29 19:35:11 +0900

    add

---
+++ 백준/Python28235.py
@@ -0,0 +1,9 @@
+text = input()
+if text == 'SONGDO':
+    print('HIGHSCHOOL')
+elif text == 'CODE':
+    print('MASTER')
+elif text == '2023':
+    print('0611')
+elif text == 'ALGORITHM':
+    print('CONTEST')
