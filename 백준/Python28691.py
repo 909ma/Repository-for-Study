d55ac323a9fe83c5b14ea0d7e123305902570462
909ma <sin00021@naver.com>
2023-10-31 20:14:22 +0900

    add

---
+++ 백준/Python28691.py
@@ -0,0 +1,11 @@
+char = input()
+if char == 'M':
+    print('MatKor')
+elif char == 'W':
+    print('WiCys')
+elif char == 'C':
+    print('CyKor')
+elif char == 'A':
+    print('AlKor')
+elif char == '$':
+    print('$clear')
