1e05e057c53265843eb727c08fca07906bfbc949
909ma <sin00021@naver.com>
2023-11-04 23:42:44 +0900

    add

---
+++ 백준/Python30087.py
@@ -0,0 +1,12 @@
+dic = {'Algorithm': 204,
+       'DataAnalysis': 207,
+       'ArtificialIntelligence': 302,
+       'CyberSecurity': 'B101',
+       'Network': 303,
+       "Startup": 501,
+       "TestStrategy": 105}
+
+test_cases = int(input())
+for _ in range(test_cases):
+    text = input()
+    print(dic[text])
