fc73bba2e69ca4060c4a9acc132de953d8bdc8a4
KIM Sinhyuk <90695479+909ma@users.noreply.github.com>
2023-11-17 16:03:59 +0900

    Update Python_추억 점수.py

--- 프로그래머스/Python_추억 점수.py
+++ 프로그래머스/Python_추억 점수.py
@@ -13,3 +13,11 @@ def solution(name, yearning, photo):
 print(solution(["may", "kein", "kain", "radi"], [5, 10, 1, 3], [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]))
 print(solution(["kali", "mari", "don"], [11, 1, 55], [["kali", "mari", "don"], ["pony", "tom", "teddy"], ["con", "mona", "don"]]))
 print(solution(["may", "kein", "kain", "radi"], [5, 10, 1, 3], [["may"],["kein", "deny", "may"], ["kon", "coni"]]))
+
+
+"""
+def solution(이름, 점수, 사진):
+    return [sum(점수[이름.index(j)] for j in i if j in 이름) for i in 사진]
+
+잘 줄여쓰네
+"""
