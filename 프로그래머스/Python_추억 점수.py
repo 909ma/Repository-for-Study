22fd676a0dff725af81da3863a5c24da7bfa75cb
KIM Sinhyuk <90695479+909ma@users.noreply.github.com>
2023-11-17 16:00:59 +0900

    Create Python_추억 점수.py

---
+++ 프로그래머스/Python_추억 점수.py
@@ -0,0 +1,15 @@
+def solution(name, yearning, photo):
+    answer = []
+    
+    for check_list in photo:
+        temp_sum = 0
+        for target, score in zip(name, yearning):
+            if target in check_list:
+                temp_sum += score
+        answer.append(temp_sum)
+    return answer
+
+
+print(solution(["may", "kein", "kain", "radi"], [5, 10, 1, 3], [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]))
+print(solution(["kali", "mari", "don"], [11, 1, 55], [["kali", "mari", "don"], ["pony", "tom", "teddy"], ["con", "mona", "don"]]))
+print(solution(["may", "kein", "kain", "radi"], [5, 10, 1, 3], [["may"],["kein", "deny", "may"], ["kon", "coni"]]))
