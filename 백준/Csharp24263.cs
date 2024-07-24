b373090174ed99ad98c4f3f12721e7471d6f04fd
KIM Sinhyuk <90695479+909ma@users.noreply.github.com>
2024-07-25 00:34:21 +0900

    Create Csharp24263.cs

---
+++ 백준/Csharp24263.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BAEKJOON {
+    class Program {
+        public static void Main(string[] args) {
+            int input = int.Parse(Console.ReadLine());
+            Console.Write($"{input}\n1");
+        }
+    }
+}
+
+// 백준에서 namespace를 `BAEKJOON`해야했던 거 같다.
