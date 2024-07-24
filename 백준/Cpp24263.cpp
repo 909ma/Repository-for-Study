b97bfe55b25bc802c9abd94f094724647d5ea37e
KIM Sinhyuk <90695479+909ma@users.noreply.github.com>
2024-07-25 00:30:42 +0900

    Create Cpp24263.cpp

---
+++ 백준/Cpp24263.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int input_num;
+    std::cin >> input_num;
+    std::cout << input_num << std::endl << "1";
+    return 0;
+}
+
+/*
+#include <iostream>을 사용하여 C++의 입출력 스트림을 포함합니다.
+std::cout을 사용하여 출력합니다. 이는 C 언어의 printf와 유사합니다.
+std::cin을 사용하여 입력을 받습니다. 이는 C 언어의 scanf와 유사합니다.
+<< 연산자를 사용하여 출력할 내용을 연결합니다. std::endl은 줄바꿈을 의미합니다.
+*/
