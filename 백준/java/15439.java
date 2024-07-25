3b47c8695e47b01641d4001ed8b509e6cfee2f6e
KIM Sinhyuk <90695479+909ma@users.noreply.github.com>
2024-07-25 20:38:11 +0900

    Create 15439.java

---
+++ 백준/java/15439.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        int n = Scanner.nextInt();
+        System.out.print(n*(n-1));
+        return;
+    }
+}
